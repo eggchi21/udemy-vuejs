9c9d68c96b2ea4db91bec2cd386c976a48576fe8
eggchi21 <eggchi21@yahoo.co.jp>
2020-01-12 17:10:43 +0900

    until sec10

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,20 @@
 <template>
-  <p>Home</p>
+  <p v-border:solid.round.shadow="{width:'5px',color: 'red'}">Home</p>
 </template>
+
+
+<script>
+  export default{
+    directives: {
+      border(el,binding){
+      el.style.border = "solid black 2px";
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg;
+      if(binding.modifiers.round){
+        el.style.borderRadius = "0.5rem"
+      }
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import LikeNumber from './components/LikeNumber'
 
 Vue.config.productionTip = false;
 Vue.component('LikeNumber',LikeNumber);
+Vue.directive("border",function(el, binding){
+  el.style.border = "solid black 2px";
+  el.style.borderWidth = binding.value.width;
+  el.style.borderColor = binding.value.color;
+  el.style.borderStyle = binding.arg;
+  if(binding.modifiers.round){
+    el.style.borderRadius = "0.5rem"
+  }
+})
 
 new Vue({
   render: h => h(App),
